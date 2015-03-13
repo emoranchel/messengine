859a0db39b243151f657c4cea921ad53cdfa3951
emoranchel@gmail.com <emoranchel@gmail.com@befcc700-846b-ab0b-a1ec-da6099684dbc>
2015-03-13 00:58:15 +0000

    Bumping to version 0.4

--- src/main/java/org/asmatron/messengine/engines/components/MessageConsumer.java
+++ src/main/java/org/asmatron/messengine/engines/components/MessageConsumer.java
@@ -1,34 +1,34 @@
 package org.asmatron.messengine.engines.components;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.asmatron.messengine.messaging.Message;
 import org.asmatron.messengine.messaging.MessageListener;
 
-
 public class MessageConsumer<T extends Message<?>> implements Runnable {
-	private final T message;
-	private final List<MessageListener<T>> listeners;
 
-	private static final Log LOG = LogFactory.getLog(MessageConsumer.class);
+    private final T message;
+    private final List<MessageListener<T>> listeners;
 
-	public MessageConsumer(T message, List<MessageListener<T>> listeners) {
-		this.message = message;
-		this.listeners = listeners;
-	}
+    private final static Logger log = Logger.getLogger(MessageConsumer.class.getName());
 
-	@Override
-	public void run() {
-		for (MessageListener<T> messageListener : listeners) {
-			try {
-				LOG.info("\nMESSAGE:"+message.getType()+":"+messageListener.getClass().getSimpleName());
-				messageListener.onMessage(message);
-			} catch (Exception e) {
-				LOG.error("Unhandled exception for a message of type " + message.getType() + " in listener " + messageListener,
-						e);
-			}
-		}
-	}
+    public MessageConsumer(T message, List<MessageListener<T>> listeners) {
+        this.message = message;
+        this.listeners = listeners;
+    }
+
+    @Override
+    public void run() {
+        for (MessageListener<T> messageListener : listeners) {
+            try {
+                log.log(Level.INFO, "\nMESSAGE:{0}:{1}", new Object[]{message.getType(), messageListener.getClass().getSimpleName()});
+                messageListener.onMessage(message);
+            } catch (Exception e) {
+                log.log(Level.SEVERE, "Unhandled exception for a message of type " + message.getType() + " in listener " + messageListener,
+                        e);
+            }
+        }
+    }
 }
--- src/main/java/org/asmatron/messengine/testing/support/TestActionRunnerFactory.java
+++ src/main/java/org/asmatron/messengine/testing/support/TestActionRunnerFactory.java
@@ -1,7 +1,7 @@
 package org.asmatron.messengine.testing.support;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.asmatron.messengine.action.Action;
 import org.asmatron.messengine.action.ActionObject;
 import org.asmatron.messengine.action.NoHandlerException;
@@ -10,47 +10,46 @@ import org.asmatron.messengine.engines.components.ActionProcessor;
 import org.asmatron.messengine.engines.components.ActionRunnable;
 import org.asmatron.messengine.engines.components.ActionRunnerFactory;
 
-
 public class TestActionRunnerFactory implements ActionRunnerFactory {
 
-	@Override
-	public <T extends ActionObject> ActionRunnable createRunner(ActionProcessor<T> commandProcessor, Action<T> command) {
-		return new ActionRunner<T>(commandProcessor, command, Thread.currentThread().getStackTrace());
-	}
+    @Override
+    public <T extends ActionObject> ActionRunnable createRunner(ActionProcessor<T> commandProcessor, Action<T> command) {
+        return new ActionRunner<T>(commandProcessor, command, Thread.currentThread().getStackTrace());
+    }
 
-	private final static Log log = LogFactory.getLog(ActionRunner.class);
+    private final static Logger log = Logger.getLogger(ActionRunner.class.getName());
 
-	class ActionRunner<T extends ActionObject> implements ActionRunnable {
+    class ActionRunner<T extends ActionObject> implements ActionRunnable {
 
-		private final ActionProcessor<T> commandProcessor;
-		private final Action<T> command;
-		private final StackTraceElement[] stack;
+        private final ActionProcessor<T> commandProcessor;
+        private final Action<T> command;
+        private final StackTraceElement[] stack;
 
-		public ActionRunner(ActionProcessor<T> commandProcessor, Action<T> command, StackTraceElement[] stack) {
-			this.commandProcessor = commandProcessor;
-			this.command = command;
-			this.stack = stack;
-		}
+        public ActionRunner(ActionProcessor<T> commandProcessor, Action<T> command, StackTraceElement[] stack) {
+            this.commandProcessor = commandProcessor;
+            this.command = command;
+            this.stack = stack;
+        }
 
-		public void run() {
-			T param = command.getParam();
-			try {
-				if (commandProcessor == null) {
-					throw new NoHandlerException("No handler found for action: " + command.getType().getId());
-				} else {
-					commandProcessor.fire(param);
-				}
-			} catch (Exception e) {
-				ActionFailedToExecute actionFailedToExecute = new ActionFailedToExecute(e, stack);
-				log.error("ACTION " + command.getType() + " FAILED!", actionFailedToExecute);
-				throw actionFailedToExecute;
-			}
-		}
+        public void run() {
+            T param = command.getParam();
+            try {
+                if (commandProcessor == null) {
+                    throw new NoHandlerException("No handler found for action: " + command.getType().getId());
+                } else {
+                    commandProcessor.fire(param);
+                }
+            } catch (Exception e) {
+                ActionFailedToExecute actionFailedToExecute = new ActionFailedToExecute(e, stack);
+                log.log(Level.SEVERE, "ACTION " + command.getType() + " FAILED!", actionFailedToExecute);
+                throw actionFailedToExecute;
+            }
+        }
 
-		@Override
-		public String getName() {
-			return command.getType().getId();
-		}
-	}
+        @Override
+        public String getName() {
+            return command.getType().getId();
+        }
+    }
 
 }
